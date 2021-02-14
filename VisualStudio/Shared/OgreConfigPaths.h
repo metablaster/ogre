451219478b2fb3f6948de2f880ca5a0b2c0c1566
metablaster <44481081+metablaster@users.noreply.github.com>
2021-02-14 23:44:17 +0100

    starting VS build configuration

---
+++ VisualStudio/Shared/OgreConfigPaths.h
@@ -0,0 +1,8 @@
+#ifndef OGRECONFIGPATHS_H_
+#define OGRECONFIGPATHS_H_
+
+#define OGRE_CONFIG_DIR "C:/dev/GitHub/ogre/out/install/x64-Debug (default)/bin"
+#define OGRE_MEDIA_DIR "C:/dev/GitHub/ogre/out/install/x64-Debug (default)/Media"
+#define OGRE_PLUGIN_DIR "C:/dev/GitHub/ogre/out/install/x64-Debug (default)/bin" 
+
+#endif /* OGRECONFIGPATHS_H_ */
