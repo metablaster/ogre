5e664ea3e2a62d25b063ba5bccbe0b3f9126ddfa
metablaster <44481081+metablaster@users.noreply.github.com>
2021-02-15 04:28:55 +0100

    add render systems projects

--- VisualStudio/Shared/OgreConfigPaths.h
+++ VisualStudio/Shared/OgreConfigPaths.h
@@ -1,8 +1,8 @@
 #ifndef OGRECONFIGPATHS_H_
 #define OGRECONFIGPATHS_H_
 
-#define OGRE_CONFIG_DIR "C:/dev/GitHub/ogre/out/install/x64-Debug (default)/bin"
-#define OGRE_MEDIA_DIR "C:/dev/GitHub/ogre/out/install/x64-Debug (default)/Media"
-#define OGRE_PLUGIN_DIR "C:/dev/GitHub/ogre/out/install/x64-Debug (default)/bin" 
+#define OGRE_CONFIG_DIR "C:/dev/GitHub/ogre/VisualStudio/Build/x64/Debug DLL"
+#define OGRE_MEDIA_DIR "C:/dev/GitHub/ogre/VisualStudio/Build/x64/Debug DLL/Media"
+#define OGRE_PLUGIN_DIR "C:/dev/GitHub/ogre/VisualStudio/Build/x64/Debug DLL"
 
 #endif /* OGRECONFIGPATHS_H_ */
